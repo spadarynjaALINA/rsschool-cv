20edb61672f43f0b0f6cb7f66591b24fb64832a0
Alina <alinagusarik@gmail.com>
2021-09-20 11:16:48 +0300

    feat: add menu, burger-menu, video, projects and some styles for adaptive

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const menuLink = document.querySelectorAll('.menu-link')
+const menuToggle = document.getElementById('#menu_toggle')
+
+menuLink.onclick = function () {
+ 
+ 
+}
+menuLink.addEventListener('click', () => {
+menuToggle.checked = true
+})
